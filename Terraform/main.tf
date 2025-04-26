cd8aaa5f93b4a7578198c4eb5959b65bbc05e7f3
kingseline <vpnc2022@gmail.com>
2025-04-26 15:34:41 +0530

    hcl

---
+++ Terraform/main.tf
@@ -0,0 +1,86 @@
+provider "aws" {
+  region = "us-east-1"  
+}
+
+# Create a VPC
+resource "aws_vpc" "hcl_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "hcl_vpc"
+  }
+}
+
+# Create a Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.hcl_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.hcl_vpc.id
+
+  tags = {
+    Name = "hcl_igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.hcl_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "main_route_table"
+  }
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# Security Group for SSH and HTTP
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.hcl_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_http"
+  }
+}
+
+
+
